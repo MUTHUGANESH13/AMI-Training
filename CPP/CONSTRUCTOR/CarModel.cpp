802ff20bf547ab91bed7879604d09e16ac6fd6c1
muthu <muthuganesh0013@gmail.com>
2026-01-24 14:03:03 +0530

    update

---
+++ CPP/CONSTRUCTOR/CarModel.cpp
@@ -0,0 +1,17 @@
+/*
+*  Program to create a Class and show the use of types of Constructor in C++
+*  Author  : MUTHUGANESH S
+*  Date    : 21/1/2026
+*  Filename: CarModel.cpp
+*  retval  : void
+*/
+
+#include <iostream>
+using namespace std;
+
+class Car{
+    string Model;
+    string Brand;
+    int MakeYear;
+    int Price;
+};
